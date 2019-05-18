117d294506d21caebf42e000b04bc61a2b7275fc
Daniel <danielgoomes31@gmail.com>
2019-05-18 18:57:48 -0300

    Evitar erro com migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Schema::defaultStringLength(191);//Evita o erro com migrations no mysql
     }
 }
