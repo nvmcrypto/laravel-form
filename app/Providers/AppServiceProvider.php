1a7b0910159540deb58e5bef79a79af92ab000b0
Daniel <danielgoomes31@gmail.com>
2019-05-18 19:21:49 -0300

    Aula 02 - Criando model e migration Client

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);//Evita o erro com migrations no mysql
+        //lISTA DE CODIGOS UTILIZADOS NO CURSO
+        //php artisan make:model Client -m - cria o model e o migration ao mesmo tempo
+        //composer dumpautoload - Deve rodar esse comando caso exclua um model e/ou migration
     }
 }
